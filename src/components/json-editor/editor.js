552b396919355b4d958c960551256fa48d283560
Renaud Genevois <renaud.genevois@insee.fr>
2019-12-18 07:30:54 +0100

    theme editor

--- src/components/json-editor/editor.js
+++ src/components/json-editor/editor.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import AceEditor from "react-ace";
 import { WPContext } from "../web-publisher";
+import "./editor.scss";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-monokai";
 
@@ -8,37 +9,39 @@ export default ({ onChange = () => null, onChangeAnnotation = () => null }) => {
   const { editorContent } = useContext(WPContext);
 
   return (
-    <AceEditor
-      placeholder="Placeholder Text"
-      mode="json"
-      theme="monokai"
-      name="blah2"
-      onLoad={editor => {
-        editor.focus();
-        editor.getSession().setUseWrapMode(true);
-        editor.getSession().on("changeAnnotation", () => {
-          onChangeAnnotation(
-            editor.getValue(),
-            editor.getSession().getAnnotations()
-          );
-        });
-        editor
-          .getSession()
-          .on("change", action => onChange(action, editor.getValue()));
-      }}
-      onChange={(...args) => {}}
-      fontSize={14}
-      showPrintMargin={true}
-      showGutter={true}
-      highlightActiveLine={true}
-      value={editorContent}
-      // setOptions={{
-      //   enableBasicAutocompletion: true,
-      //   enableLiveAutocompletion: false,
-      //   enableSnippets: false,
-      //   showLineNumbers: true,
-      //   tabSize: 2
-      // }}
-    />
+    <div className="editor">
+      <AceEditor
+        placeholder="Placeholder Text"
+        mode="json"
+        theme="monokai"
+        name="blah2"
+        onLoad={editor => {
+          editor.focus();
+          editor.getSession().setUseWrapMode(false);
+          editor.getSession().on("changeAnnotation", () => {
+            onChangeAnnotation(
+              editor.getValue(),
+              editor.getSession().getAnnotations()
+            );
+          });
+          editor
+            .getSession()
+            .on("change", action => onChange(action, editor.getValue()));
+        }}
+        onChange={(...args) => {}}
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={editorContent}
+        // setOptions={{
+        //   enableBasicAutocompletion: true,
+        //   enableLiveAutocompletion: false,
+        //   enableSnippets: false,
+        //   showLineNumbers: true,
+        //   tabSize: 2
+        // }}
+      />
+    </div>
   );
 };
--- src/components/web-publisher/web-publisher.js
+++ src/components/web-publisher/web-publisher.js
@@ -1,63 +1,60 @@
-import React, { useEffect, useReducer } from 'react';
-import reducer, { initial } from './reducer';
-import { Panel } from '../commons';
-import JsonEditor from '../json-editor';
-import browserSource from './tools/browse-source';
-import validateSource from './tools/validate-source';
-import * as actions from './actions';
-import GLOBAL_LISTENER from 'utils/global-listeners';
-import AppContext from './context';
-import Errors from './errors';
-import './web-publisher.scss';
+import React, { useEffect, useReducer } from "react";
+import reducer, { initial } from "./reducer";
+import { Panel } from "../commons";
+import JsonEditor from "../json-editor";
+import browserSource from "./tools/browse-source";
+import validateSource from "./tools/validate-source";
+import * as actions from "./actions";
+import GLOBAL_LISTENER from "utils/global-listeners";
+import AppContext from "./context";
+import Errors from "./errors";
+import "./web-publisher.scss";
 
-const loadSource = () => fetch('/source.json').then((res) => res.json());
+const loadSource = () => fetch("/source.json").then(res => res.json());
 
-const stringifySource = (source) => JSON.stringify(source, null, '\t');
+const stringifySource = source => JSON.stringify(source, null, "\t");
 
 export default () => {
-	const [ state, dispatch ] = useReducer(reducer, initial);
-	const { init, source, errors } = state;
-	useEffect(
-		() => {
-			if (!init) {
-				dispatch(actions.init());
-				loadSource().then((src) => {
-					dispatch(actions.setSource(src));
-					dispatch(actions.setEditorContent(stringifySource(src)));
-				});
-				return () => {
-					if (init) {
-						GLOBAL_LISTENER.clean();
-					}
-				};
-			}
-		},
-		[ init ]
-	);
-	return (
-		<AppContext.Provider value={{ ...state, dispatch }}>
-			<div className="web-publisher">
-				<Panel className="editor-panel" top={50} left={100}>
-					<div className="editor">
-						<JsonEditor
-							onChangeAnnotation={(content, annotations) => {
-								if (!annotations.length) {
-									const src = JSON.parse(content);
-									dispatch(actions.setSource(src));
-									dispatch(actions.setEditorContent(content));
-									const err = validateSource(src);
-									console.log(err);
-									dispatch(actions.setErrors(err));
-								} else {
-									dispatch(actions.setErrors(annotations));
-									dispatch(actions.setEditorContent(content));
-								}
-							}}
-						/>
-					</div>
-				</Panel>
-				<div className="wp-content">{errors.length ? <Errors /> : browserSource(source)}</div>
-			</div>
-		</AppContext.Provider>
-	);
+  const [state, dispatch] = useReducer(reducer, initial);
+  const { init, source, errors } = state;
+  useEffect(() => {
+    if (!init) {
+      dispatch(actions.init());
+      loadSource().then(src => {
+        dispatch(actions.setSource(src));
+        dispatch(actions.setEditorContent(stringifySource(src)));
+      });
+      return () => {
+        if (init) {
+          GLOBAL_LISTENER.clean();
+        }
+      };
+    }
+  }, [init]);
+  return (
+    <AppContext.Provider value={{ ...state, dispatch }}>
+      <div className="web-publisher">
+        <Panel className="editor-panel" top={50} left={100}>
+          <JsonEditor
+            onChangeAnnotation={(content, annotations) => {
+              if (!annotations.length) {
+                const src = JSON.parse(content);
+                dispatch(actions.setSource(src));
+                dispatch(actions.setEditorContent(content));
+                const err = validateSource(src);
+                console.log(err);
+                dispatch(actions.setErrors(err));
+              } else {
+                dispatch(actions.setErrors(annotations));
+                dispatch(actions.setEditorContent(content));
+              }
+            }}
+          />
+        </Panel>
+        <div className="wp-content">
+          {errors.length ? <Errors /> : browserSource(source)}
+        </div>
+      </div>
+    </AppContext.Provider>
+  );
 };
